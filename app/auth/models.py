df2bdacbec8bff0cd0900dee19d0982f0dfa5077
Jaha-hub <nuratdinovzahangir4@gmail.com>
2026-02-25 17:07:02 +0500

    5

---
+++ app/auth/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, String, Text
+
+from app.core.models import Base, IntIdMixin, TimeActionMixin
+
+
+class User(Base, IntIdMixin,TimeActionMixin):
+    """
+
+    """
+    __tablename__ = "users"
+    fullname = Column(String(512), nullable=False)
+    role = Column(String(20), nullable=False, default="user")
+    username = Column(String(320), nullable=False, unique=True)
+    hashed_password = Column(Text, nullable=False)
