c1b97d3420d08e3468116708631c2a3a2cf36816
Jaha-hub <nuratdinovzahangir4@gmail.com>
2026-02-25 17:05:04 +0500

    4

---
+++ app/core/settings.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+class Settings(BaseSettings):
+    ACCESS_EXPIRES: int
+    REFRESH_EXPIRES: int
+    TOKEN_ALGORITHM: str
+    TOKEN_SECRET_KEY: str
+
+    ASYNC_DATABASE_URL: str
+
+    DATABASE_URL: str
+
+    POSTGRES_DB: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+
+    BACKEND_URL: str
+
+
+
+    model_config = SettingsConfigDict(
+        env_file=".env"
+    )
+
+
+settings = Settings()
