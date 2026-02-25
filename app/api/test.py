b7483f2f09f767cefe4fbcc6b4103290d547a453
L0bster56 <sanjaranvarov55563@gmail.com>
2026-02-25 16:20:51 +0500

    api1

---
+++ app/api/test.py
@@ -0,0 +1,23 @@
+import httpx
+
+url = "https://api.exchangerate.host/live"
+
+API_key = "6c6afbe59e93916599b1977d10db6252"
+
+
+def get_list():
+    params = {
+        "access_key": API_key,
+        "currencies": "USD, EUR, JPY, CNH,  UZS, RUB, CAD",
+    }
+
+    response = httpx.get(url, params=params)
+    data = response.json()
+    return data["quotes"]
+
+
+rates = get_list()
+
+for pair, value in rates.items():
+    print(F"1 {pair[3:]} = {value} {pair[:3]}")
+
