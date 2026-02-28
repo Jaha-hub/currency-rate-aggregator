0db9101484672133876a9a7d13fe6ed45708a165
Jaha-hub <nuratdinovzahangir4@gmail.com>
2026-02-28 13:14:27 +0500

    9

--- app/balance/models.py
+++ app/balance/models.py
@@ -8,3 +8,4 @@ class Balance(Base, IntIdMixin, TimeActionMixin):
     user_id = Column(BigInteger, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
     currency = Column(BigInteger)
     sum = Column(Numeric(16,2))
+
